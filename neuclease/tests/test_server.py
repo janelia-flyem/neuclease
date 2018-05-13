98cbdf15b238648369d22f1fddbce6977f23a929
Stuart Berg <bergs@janelia.hhmi.org>
2018-05-12 22:40:39 -0400

    test_server: Fix exception handling logic

--- neuclease/tests/test_server.py
+++ neuclease/tests/test_server.py
@@ -60,8 +60,8 @@ def test_simple_request(cleave_server_setup):
              "segmentation-instance": "segmentation",
              "mesh-instance": "segmentation_meshes_tars" }
 
+    r = requests.post(f'http://127.0.0.1:{port}/compute-cleave', json=data)
     try:
-        r = requests.post(f'http://127.0.0.1:{port}/compute-cleave', json=data)
         r.raise_for_status()
     except requests.RequestException:
         sys.stderr.write(r.content.decode() + '\n')
@@ -79,8 +79,8 @@ def test_fetch_log(cleave_server_setup):
     """
     _dvid_server, _dvid_port, _dvid_repo, port = cleave_server_setup
 
+    r = requests.get(f'http://127.0.0.1:{port}')
     try:
-        r = requests.get(f'http://127.0.0.1:{port}')
         r.raise_for_status()
     except requests.RequestException:
         sys.stderr.write(r.content.decode() + '\n')
