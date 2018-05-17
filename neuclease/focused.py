ea73fdec936971eeb210129956d3ed794d06a104
Stuart Berg <bergs@janelia.hhmi.org>
2018-05-16 21:09:30 -0400

    focused: Add options for faster debugging (limit search)

--- neuclease/focused.py
+++ neuclease/focused.py
@@ -52,7 +52,15 @@ def main():
 
 
 DvidInstanceInfo = namedtuple("DvidInstanceInfo", "server uuid instance")
-def compute_focused_paths(instance_info, original_mapping, important_bodies, speculative_merge_tables, split_mapping=None, max_depth=10):
+def compute_focused_paths( instance_info,
+                           original_mapping,
+                           important_bodies,
+                           speculative_merge_tables,
+                           split_mapping=None,
+                           max_depth=10,
+                           stop_after_endpoint_num=None,
+                           return_after_setup=False ):
+
     instance_info = DvidInstanceInfo(*instance_info)
     with Timer("Loading speculative merge graph", logger):
         merge_graph = LabelmapMergeGraph(speculative_merge_tables, instance_info.uuid)   
@@ -94,49 +102,59 @@ def compute_focused_paths(instance_info, original_mapping, important_bodies, spe
     edges = merge_table_df[['id_a', 'id_b']].values
     assert edges.dtype == np.uint64
 
+    if return_after_setup:
+        logger.info("Returning setup instead of searching for paths")
+        return (edges, original_mapping, important_bodies, max_depth, stop_after_endpoint_num)
+
+    logger.info(f"Finding paths among {len(important_bodies)} important bodies")
     # FIXME: Need to augment original_mapping with rows for single-sv bodies that are 'important'
-    all_paths = find_all_paths(edges, original_mapping, important_bodies, max_depth)
+    all_paths = find_all_paths(edges, original_mapping, important_bodies, max_depth, stop_after_endpoint_num)
     return all_paths
 
 
-def find_all_paths(edges, original_mapping, important_bodies, max_depth=10):
+def find_all_paths(edges, original_mapping, important_bodies, max_depth=10, stop_after_endpoint_num=None):
     """
     Returns a dict of { start_sv: VertexPath }
     """
-    # Load into Graph
-    import graph_tool as gt
-    g = gt.Graph(directed=False)
-    sv_pmap = g.add_edge_list( edges, hashed=True )
+    with Timer("Loading into gt.Graph", logger):
+        import graph_tool as gt
+        g = gt.Graph(directed=False)
+        sv_pmap = g.add_edge_list( edges, hashed=True )
 
     # Mappings from Graph vertices (which are consecutive) to SV ids and vice-versa
     v_to_sv = sv_pmap.get_array().astype(np.uint64)
     sv_to_v = pd.Series(index=v_to_sv, data=np.arange(len(v_to_sv), dtype=np.uint32))
 
-    sv_importances = pd.DataFrame(original_mapping)
-    sv_importances['v'] = sv_to_v.loc[original_mapping.index]
-    sv_importances['important'] = original_mapping.index.isin(important_bodies)
+    with Timer("Mapping SV importances"):
+        sv_importances = pd.DataFrame(original_mapping)
+        sv_importances['v'] = sv_to_v.loc[original_mapping.index]
+        sv_importances['important'] = original_mapping.index.isin(important_bodies)
         
-    important_svs = sv_importances[sv_importances['important']].index
-    important_verts = pd.Index(sv_importances[sv_importances['important']]['v'])
+        important_svs = sv_importances[sv_importances['important']].index
+        important_verts = pd.Index(sv_importances[sv_importances['important']]['v'])
 
-    all_paths = {}
-    for sv in important_svs:
-        v = sv_to_v.loc[sv]
-        v_paths = []
-        find_paths_from( max_depth,
-                         important_verts,
-                         g,
-                         v,
-                         v_paths,
-                         current_path=[v],
-                         current_path_edge_ids=[None],
-                         current_depth=0 )
+    if stop_after_endpoint_num is None:
+        stop_after_endpoint_num = len(important_svs)
 
-        # map v_paths to sv_paths
-        sv_paths = []
-        for v_path in v_paths:
-            sv_paths.append(v_to_sv[v_path.vertices])
-        all_paths[sv] = sv_paths
+    with Timer(f"Finding paths from {stop_after_endpoint_num} endpoints", logger):
+        all_paths = {}
+        for sv in important_svs[:stop_after_endpoint_num]:
+            v = sv_to_v.loc[sv]
+            v_paths = []
+            find_paths_from( max_depth,
+                             important_verts,
+                             g,
+                             v,
+                             v_paths,
+                             current_path=[v],
+                             current_path_edge_ids=[None],
+                             current_depth=0 )
+    
+            # map v_paths to sv_paths
+            sv_paths = []
+            for v_path in v_paths:
+                sv_paths.append(v_to_sv[v_path.vertices])
+            all_paths[sv] = sv_paths
 
     return all_paths
 
