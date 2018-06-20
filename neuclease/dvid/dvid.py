9fbafa46bb0e8da60717ef16269f619b3d5bcf5b
Stuart Berg <bergs@janelia.hhmi.org>
2018-06-20 00:07:08 -0400

    Added dvid.fetch_sparsevol_coarse (migrated from DVIDSparkServices)

--- neuclease/dvid/dvid.py
+++ neuclease/dvid/dvid.py
@@ -9,6 +9,7 @@ import requests
 
 import numpy as np
 import pandas as pd
+from numba import jit
 
 from ..util import Timer
 
@@ -135,6 +136,7 @@ def extract_first_rle_coord(rle_payload_bytes):
     first_coord_zyx = first_coord_xyz[::-1]
     return first_coord_zyx
 
+
 @sanitize_server
 def split_supervoxel(instance_info, supervoxel, rle_payload_bytes):
     """
@@ -146,7 +148,7 @@ def split_supervoxel(instance_info, supervoxel, rle_payload_bytes):
     server, uuid, instance = instance_info
     session = default_dvid_session()
     r = session.post(f'http://{server}/api/node/{uuid}/{instance}/split-supervoxel/{supervoxel}', data=rle_payload_bytes)
-    r.raise_for_status()   
+    r.raise_for_status()
     
     results = r.json()
     return (results["SplitSupervoxel"], results["RemainSupervoxel"] )
@@ -203,14 +205,132 @@ def fetch_mutation_id(instance_info, body_id):
     return r.json()["mutation id"]
 
 
+@sanitize_server
+def fetch_sparsevol_coarse(instance_info, body_id):
+    """
+    Return the 'coarse sparsevol' representation of a given body.
+    This is similar to the sparsevol representation at scale=6,
+    EXCEPT that it is generated from the label index, so no blocks
+    are lost from downsampling.
+
+    Return an array of coordinates of the form:
+
+        [[Z,Y,X],
+         [Z,Y,X],
+         [Z,Y,X],
+         ...
+        ]
+    """
+    server, uuid, instance = instance_info
+    session = default_dvid_session()
+    r = session.get(f'{server}/api/node/{uuid}/{instance}/sparsevol-coarse/{body_id}')
+    r.raise_for_status()
+    
+    return parse_rle_response( r.content )
+
+
+def parse_rle_response(response_bytes):
+    """
+    Parse a (legacy) RLE response from DVID, used by various endpoints
+    such as 'sparsevol' and 'sparsevol-coarse'.
+    
+    Return an array of coordinates of the form:
+
+        [[Z,Y,X],
+         [Z,Y,X],
+         [Z,Y,X],
+         ...
+        ]
+    """
+    descriptor = response_bytes[0]
+    ndim = response_bytes[1]
+    run_dimension = response_bytes[2]
+
+    assert descriptor == 0, f"Don't know how to handle this payload. (descriptor: {descriptor})"
+    assert ndim == 3, "Expected XYZ run-lengths"
+    assert run_dimension == 0, "FIXME, we assume the run dimension is X"
+
+    content_as_int32 = np.frombuffer(response_bytes, np.int32)
+    _voxel_count = content_as_int32[1]
+    run_count = content_as_int32[2]
+    rle_items = content_as_int32[3:].reshape(-1,4)
+
+    assert len(rle_items) == run_count, \
+        f"run_count ({run_count}) doesn't match data array length ({len(rle_items)})"
+
+    rle_starts_xyz = rle_items[:,:3]
+    rle_starts_zyx = rle_starts_xyz[:,::-1]
+    rle_lengths = rle_items[:,3]
+
+    # Sadly, the decode function requires contiguous arrays, so we must copy.
+    rle_starts_zyx = rle_starts_zyx.copy('C')
+    rle_lengths = rle_lengths.copy('C')
+
+    # For now, DVID always returns a voxel_count of 0, so we can't make this assertion.
+    #assert rle_lengths.sum() == _voxel_count,\
+    #    f"Voxel count ({voxel_count}) doesn't match expected sum of run-lengths ({rle_lengths.sum()})"
+
+    dense_coords = runlength_decode_from_lengths(rle_starts_zyx, rle_lengths)
+    
+    assert rle_lengths.sum() == len(dense_coords), "Got the wrong number of coordinates!"
+    return dense_coords
+
+
+@jit("i4[:,:](i4[:,::1],i4[::1])", nopython=True) # See note about signature, below.
+def runlength_decode_from_lengths(rle_start_coords_zyx, rle_lengths):
+    """
+    Given a 2D array of coordinates and a 1D array of runlengths, i.e.:
+        
+        [[Z,Y,X], [Z,Y,X], [Z,Y,X],...]
+
+        and 
+        
+        [Length, Length, Length,...]
+
+    Return an array of coordinates of the form:
+
+        [[Z,Y,X],
+         [Z,Y,X],
+         [Z,Y,X],
+         ...
+        ]
+    
+    In which every run-length has been expanded into a run
+    of consecutive coordinates in the result.
+    That is, result.shape == (rle_lengths.sum(), 3)
+    
+    Note: The "runs" are expanded along the X AXIS.
+    
+    Note about Signature:
+    
+        Due to an apparent numba bug, it is dangerous to pass non-contiguous arrays to this function.
+        (It returns incorrect results.)
+        
+        Therefore, the signature is explicitly written above to require contiguous arrays (e.g. i4[::1]),
+        If you attempt to pass a non-contiguous array, you'll see an error like this:
+        
+            TypeError: No matching definition for argument type(s) readonly array(int32, 2d, A), readonly array(int32, 1d, C)
+    """
+    # Numba doesn't allow us to use empty lists at all,
+    # so we have to initialize this list with a dummy row,
+    # which we'll omit in the return value
+    coords = [0,0,0]
+    for i in range(len(rle_start_coords_zyx)):
+        (z, y, x0) = rle_start_coords_zyx[i]
+        length = rle_lengths[i]
+        for x in range(x0, x0+length):
+            coords.extend([z,y,x])
+
+    coords = np.array(coords, np.int32).reshape((-1,3))
+    return coords[1:, :] # omit dummy row (see above)
+
+
 def compute_changed_bodies(instance_info_a, instance_info_b):
     """
     Returns the list of all bodies whose supervoxels changed
     between uuid_a and uuid_b.
     This includes bodies that were changed, added, or removed completely.
     """
-    instance_info_a
-    
     mapping_a = fetch_mappings(instance_info_a)
     mapping_b = fetch_mappings(instance_info_b)
     
