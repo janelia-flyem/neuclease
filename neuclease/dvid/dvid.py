3e4b454b7ab5b4ee3ebc2dfe3b947f386e948456
Stuart Berg <bergs@janelia.hhmi.org>
2018-07-02 18:51:27 -0400

    Added dvid.create_branch()

--- neuclease/dvid/dvid.py
+++ neuclease/dvid/dvid.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import logging
@@ -83,6 +82,29 @@ def fetch_generic_json(url, json=None):
     r.raise_for_status()
     return r.json()
 
+@sanitize_server
+def create_branch(server, uuid, branch_name, note=None, custom_uuid=None):
+    """
+    Create a branch from the given UUID with the given new branch name.
+    Branch name must be unique (not used previously in the repo).
+    
+    If a custom UUID is provided, it will be used by DVID instead of auto-generating one.
+    Note: Providing a custom UUID is unusual and should probably be avoided.
+
+    Returns:
+        The uuid of the new branch.
+    """
+    session = default_dvid_session()
+    body = {"branch": branch_name}
+    if note:
+        body["note"] = note
+
+    if custom_uuid:
+        body["uuid"] = custom_uuid
+        
+    r = session.post(f'http://{server}/api/node/{uuid}/branch', json=body)
+    r.raise_for_status()
+    return r.json()["child"]
 
 @sanitize_server
 def fetch_key(instance_info, key, as_json=False):
