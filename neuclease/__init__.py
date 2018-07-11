8b714f233af30ade0ff333451bc469f06f7b8946
Stuart Berg <bergs@janelia.hhmi.org>
2018-07-11 11:09:39 -0400

    Added neuclease.configure_default_logging()

--- neuclease/__init__.py
+++ neuclease/__init__.py
@@ -1,6 +1,19 @@
+import sys
 import faulthandler
 if not faulthandler.is_enabled():
     faulthandler.enable()
 
 import logging
 logging.getLogger('kafka').setLevel(logging.WARNING)
+
+def configure_default_logging():
+    """
+    Simple logging configuration.
+    Useful for interactive terminal sessions.
+    """
+    handler = logging.StreamHandler(sys.stdout)
+
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+    root_logger.addHandler(handler)
+    root_logger.setLevel(logging.INFO)
