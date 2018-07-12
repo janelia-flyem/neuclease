95d16eb45ff3e8ee50a06171a339654404396672
Stuart Berg <bergs@janelia.hhmi.org>
2018-07-12 09:27:41 -0400

    dvid: Added fetch_file() and fetch_tarfile()

--- neuclease/dvid/dvid.py
+++ neuclease/dvid/dvid.py
@@ -1317,6 +1317,73 @@ def fetch_supervoxel_fragments(instance_info, split_source='kafka'):
 
 
 @sanitize_server
+def fetch_tarfile(instance_info, body_id, output=None):
+    """
+    Fetch a .tar file from a tarsupervoxels instance for the given body,
+    and save it to bytes, a file object, or a file path.
+    
+    Args:
+        instance_info:
+            server, uuid, instance
+        
+        body_id:
+            The body whose supervoxel files will be fetched in the tar.
+        
+        output:
+            If None, tarfile is returned in-memory, as bytes.
+            If str, it is interpreted as a path to which the .tar file will be written.
+            Otherwise, must be a file object to write the bytes to (e.g. a BytesIO object).
+    
+    Returns:
+        None, unless no output file object/path is provided,
+        in which case the tarfile bytes are returned.
+    """
+    server, uuid, instance = instance_info
+    url = f'http://{server}/api/node/{uuid}/{instance}/tarfile/{body_id}'
+    return fetch_file(url, output)
+
+
+def fetch_file(url, output=None, chunksize=2**10):
+    """
+    Fetch a file from the given endpoint,
+    and save it to bytes, a file object, or a file path.
+
+    Args:
+        url:
+            Complete url to fetch from.
+        
+        output:
+            If None, file is returned in-memory, as bytes.
+            If str, it is interpreted as a path to which the file will be written.
+            Otherwise, must be a file object to write the bytes to (e.g. a BytesIO object).
+        
+        chunksize:
+            Data will be streamed in chunks, with the given chunk size.
+
+    Returns:
+        None, unless no output file object/path is provided,
+        in which case the fetched bytes are returned.
+    """
+    session = default_dvid_session()
+
+    with session.get(url, stream=True) as r:
+        r.raise_for_status()
+
+        if output is None:
+            return r.content
+
+        if isinstance(output, str):
+            # Create a file on disk and write to it.
+            with open(output, 'wb') as f:
+                for chunk in r.iter_content(chunksize):
+                    f.write(chunk)
+        else:
+            # output is a file object
+            for chunk in r.iter_content(chunksize):
+                output.write(chunk)
+
+
+@sanitize_server
 def fetch_labelindex(instance_info, label, format='protobuf'): # @ReservedAssignment
     """
     Fetch the LabelIndex for the given label ID from DVID,
