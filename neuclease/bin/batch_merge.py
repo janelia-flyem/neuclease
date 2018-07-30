2b1a46412dbe72cc2c447fa8cee0533faa8b3405
Stuart Berg <bergs@janelia.hhmi.org>
2018-07-30 17:29:03 -0400

    batch_merge: Return a list of all merge sets that contained multiple differently named bodies, and some cleanups

--- neuclease/bin/batch_merge.py
+++ neuclease/bin/batch_merge.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import networkx as nx
 from neuclease.dvid import fetch_mapping, fetch_sizes, post_merge
@@ -14,39 +15,54 @@ def post_merges(instance_info, sv_merges):
             (server, uuid, segmentation_name)
         sv_merges:
             list/array of supervoxel pairs to merge
+
+    Returns:
+        If any merged sets contained multple named bodies whose names did not match,
+        the merge is posted anyway, but a list of such merge sets is returned.
     """
-    df = pd.DataFrame(sv_merges, columns=['id_a', 'id_b'])
-    df['body_a'] = fetch_mapping(instance_info, sv_merges['id_a'].values)
-    df['body_b'] = fetch_mapping(instance_info, sv_merges['id_b'].values)
+    sv_merges = np.asarray(sv_merges)
+    bodies_a = fetch_mapping(instance_info, sv_merges[:,0])
+    bodies_b = fetch_mapping(instance_info, sv_merges[:,1])
+
+    assert 0 not in bodies_a, "Some supervoxels no longer exist"
+    assert 0 not in bodies_b, "Some supervoxels no longer exist"
     
     # Build graph of bodies to merge
     g = nx.Graph()
-    g.add_edges_from(df[['body_a', 'body_b']].values)
+    g.add_edges_from(zip(bodies_a, bodies_b))
     
-    # Pull out bodies in connected groups
+    # Extract bodies in connected groups
+    flagged_merge_sets = []
     for body_set in nx.connected_components(g):
-        bodies = list(body_set)
-        sizes = fetch_sizes(instance_info, bodies)
-        merge_df = pd.DataFrame(sizes, columns=['size'], index=bodies)
+        body_list = list(body_set)
+        sizes = fetch_sizes(instance_info, body_list)
+        merge_df = pd.DataFrame(sizes, columns=['size'], index=body_list)
         
         # FIXME: How do I fetch body names?
         merge_df['name'] = ''
         
-        rows_with_name = merge_df.query(" name != '' ")
-    
-        if len(rows_with_name) > 0:
+        rows_with_name = merge_df.query(" name != '' ")    
+        if len(rows_with_name) == 0:
+            # No names; choose biggest body
+            main_body = merge_df['size'].idxmax()
+        else:
             # Choose biggest NAMED body
             main_body = rows_with_name['size'].idxmax()
-        else:
-            # Choose biggest body
-            main_body = merge_df['size'].idxmax()
+
+            # Are there multiple names in this set?
+            if rows_with_name['name'].nunique() > 1:
+                flagged_merge_sets.append(body_set)
     
+        # Send to DVID
         other_bodies = body_set - {main_body}
-    
-        # Send to DVID    
         post_merge(instance_info, main_body, list(other_bodies))
+        
+    return flagged_merge_sets
+
 
 if __name__ == "__main__":
+    # Exmaple usage.
+    #
     # FIXME: Load actual SV merge table from completed assignments
     sv_merges = [[1,2],
                  [1,3],
@@ -57,4 +73,4 @@ if __name__ == "__main__":
                  [8,9]]
 
     instance_info = ('emdata3:8900', '7e52', 'segmentation')
-    post_merges(instance_info, sv_merges)
+    flagged_sets = post_merges(instance_info, sv_merges)
