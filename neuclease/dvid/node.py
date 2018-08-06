9ea7cd15e81195a1696516fc1f7840b31e5d250b
Stuart Berg <bergs@janelia.hhmi.org>
2018-08-06 10:43:35 -0400

    Added post_commit(), fetch_commit(), post_newversion()

--- neuclease/dvid/node.py
+++ neuclease/dvid/node.py
@@ -10,6 +10,36 @@ def fetch_full_instance_info(server, uuid, instance, *, session=None):
 
 
 @dvid_api_wrapper
+def post_commit(server, uuid, note, log=[], *, session=None):
+    """
+    Commit (a.k.a. lock) the given node with a commit note and optional log.
+    Returns the full UUID of the committed node.
+    """
+    body = { "note": note }
+    if log:
+        assert isinstance(log, list)
+        for item in log:
+            assert isinstance(item, str)
+        body["log"] = log
+    
+    r = session.post(f'http://{server}/api/node/{uuid}/commit', json=body)
+    r.raise_for_status()
+    return r.json()["committed"]
+
+
+@dvid_api_wrapper
+def fetch_commit(server, uuid, *, session=None):
+    """
+    Returns the locked status (True/False) if the given node.
+    
+    Returns True if the node is locked.
+    """
+    r = session.get(f'http://{server}/api/node/{uuid}/commit')
+    r.raise_for_status()
+    return r.json()["Locked"]
+    
+
+@dvid_api_wrapper
 def post_branch(server, uuid, branch_name, note, custom_uuid=None, *, session=None):
     """
     Create a branch from the given UUID with the given new branch name.
@@ -19,11 +49,10 @@ def post_branch(server, uuid, branch_name, note, custom_uuid=None, *, session=No
     Note: Providing a custom UUID is unusual and should probably be avoided.
 
     Returns:
-        The uuid of the new branch.
+        The uuid of the new node (the start of the new branch).
     """
     body = {"branch": branch_name}
-    if note:
-        body["note"] = note
+    body["note"] = note
 
     if custom_uuid:
         body["uuid"] = custom_uuid
@@ -33,4 +62,21 @@ def post_branch(server, uuid, branch_name, note, custom_uuid=None, *, session=No
     return r.json()["child"]
 
 
+@dvid_api_wrapper
+def post_newversion(server, uuid, note, custom_uuid=None, *, session=None):
+    """
+    Creates a new node on the same branch as the given uuid.
 
+    If a custom UUID is provided, it will be used by DVID instead of auto-generating one.
+    Note: Providing a custom UUID is unusual and should probably be avoided.
+
+    Returns:
+        uuid of the new node
+    """
+    body = { "note": note }
+    if custom_uuid:
+        body["uuid"] = custom_uuid
+        
+    r = session.post(f'http://{server}/api/node/{uuid}/newversion', json=body)
+    r.raise_for_status()
+    return r.json()["child"]
