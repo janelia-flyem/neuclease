12d2cffc87f193484df482aaf90da39f0cc08e27
Stuart Berg <bergs@janelia.hhmi.org>
2018-08-09 16:01:02 -0400

    production command

--- flyem-production-commands.sh
+++ flyem-production-commands.sh
@@ -7,7 +7,7 @@
 ACTIVATE=/groups/flyem/proj/cluster/miniforge/bin/activate
 source $ACTIVATE flyem
 
-# emdata3:8900 -- 2053c1a64f254961874d91407e7301e3
+# emdata3:8900 -- d5852c27b5c04687bb1be414f6dc2336
 # Run this on emdata3 to produce a cleave server on emdata3:5551
 
 nohup neuclease_cleave_server \
@@ -15,7 +15,7 @@ nohup neuclease_cleave_server \
   --log-dir /nrs/flyem/bergs/neuclease-logs/production-emdata3-8900 \
   --merge-table /nrs/flyem/bergs/final-agglo-fixsplit-patched/final_patched_20180426_merge_table.npy \
   --primary-dvid-server emdata3:8900 \
-  --primary-uuid 2053c1a64f254961874d91407e7301e3 \
+  --primary-uuid d5852c27b5c04687bb1be414f6dc2336 \
   --primary-labelmap-instance segmentation \
   &
 ##
