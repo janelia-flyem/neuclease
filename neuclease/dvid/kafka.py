d7b72315ddf850fffbd117c01066098c9e91d182
Stuart Berg <bergs@janelia.hhmi.org>
2018-09-05 09:45:57 -0400

    dvid.kafka.read_kafka_messages(): Cleanup/refactor to reduce function length.

--- neuclease/dvid/kafka.py
+++ neuclease/dvid/kafka.py
@@ -14,14 +14,20 @@ from .node import fetch_instance_info
 
 logger = logging.getLogger(__name__)
 
+
 class KafkaReadError(RuntimeError):
     pass
+
 
 @dvid_api_wrapper
 def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='leaf-and-parents', return_format='json-values', group_id=None, consumer_timeout=2.0, *, session=None):
     """
     Read the stream of available Kafka messages for the given DVID instance,
-    and optionally filter them by UUID or Action.
+    with convenient filtering options.
+
+    This function reads from the correct kafka server(s) (as listed by DVID),
+    and will (optionally) filter out messages from DVID nodes that are not
+    upstream of the given UUID, or don't match the given set of dvid 'actions'.
 
     Args:
         server:
@@ -50,8 +56,11 @@ def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='
             or 'json-values' (return list of parsed JSON structures from each record.value)
 
         group_id:
-            Kafka group ID to use when reading.  If not given, a new one is created.
-            (FIXME: Frequently creating new group IDs like this is probably not best-practice, but it works for now.)
+            Kafka group ID to use when reading.  If not given, a new unique ID is created
+            to ensure that the complete log is read.
+            Note: If you re-use group IDs, then subsequent calls to this function will
+                  not yield repeated messages.  The log will resume where it left off
+                  from the previous call.
         
         consumer_timeout:
             Seconds to timeout (after which we assume we've read all messages).
@@ -59,18 +68,10 @@ def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='
     Returns:
         Filtered list of kafka ConsumerRecord or list of JSON dict (depending on return_format).
     """
-    from kafka import KafkaConsumer
-    
     assert dag_filter in ('leaf-only', 'leaf-and-parents', None)
     assert return_format in ('records', 'json-values')
 
-    if group_id is None:
-        # Choose a unique 'group_id' to use
-        # FIXME: Frequently creating new group IDs like this is probably not best-practice, but it works for now.
-        group_id = getpass.getuser() + '-' + datetime.now().isoformat()
-    
     server_info = fetch_server_info(server, session=session)
-
     if "Kafka Servers" not in server_info or not server_info["Kafka Servers"]:
         raise KafkaReadError(f"DVID server ({server}) does not list a kafka server")
 
@@ -80,12 +81,51 @@ def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='
     data_uuid = full_instance_info["Base"]["DataUUID"]
     repo_uuid = full_instance_info["Base"]["RepoUUID"]
 
-    consumer = KafkaConsumer( f'dvidrepo-{repo_uuid}-data-{data_uuid}',
+    logger.info(f"Reading kafka messages from {kafka_servers} for {server} / {uuid} / {instance}")
+    with Timer() as timer:
+        records = _read_complete_kafka_log(f'dvidrepo-{repo_uuid}-data-{data_uuid}',
+                                           kafka_servers, group_id, consumer_timeout)
+    logger.info(f"Reading {len(records)} kafka messages took {timer.seconds} seconds")
+
+    # Extract and parse JSON values for easy filtering.
+    values = [json.loads(rec.value) for rec in records]
+    records_and_values = zip(records, values)
+
+    # Chain filters and evaluate
+    records_and_values = _filter_records_for_dag(records_and_values, dag_filter, server, repo_uuid, uuid, session)
+    records_and_values = _filter_records_for_action(records_and_values, action_filter)
+    records_and_values = list(records_and_values)
+
+    if return_format == 'records':
+        return [record for (record, _value) in records_and_values]
+    elif return_format == 'json-values':
+        return [value for (_record, value) in records_and_values]
+    else:
+        raise AssertionError(f"Invalid return_format: {return_format}")
+
+
+def _read_complete_kafka_log(topic, kafka_servers, group_id=None, timeout_seconds=2.0):
+    """
+    Helper function.
+    Read the complete kafka log for the given topic.
+    Return a list of ConsumerRecord objects.
+    
+    Special care is taken to ensure that the complete log was read.
+    An error is raised if it appears that the log was terminated early.
+    """
+    from kafka import KafkaConsumer
+
+    if group_id is None:
+        # Choose a unique 'group_id' to use
+        # FIXME: Frequently creating new group IDs like this is probably not best-practice, but it works for now.
+        group_id = getpass.getuser() + '-' + datetime.now().isoformat()
+    
+    consumer = KafkaConsumer( topic,
                               bootstrap_servers=kafka_servers,
                               group_id=group_id,
                               enable_auto_commit=False,
                               auto_offset_reset='earliest',
-                              consumer_timeout_ms=int(consumer_timeout * 1000))
+                              consumer_timeout_ms=int(timeout_seconds * 1000))
     
     # There seems to be some sort of timing issue.
     # If we start fetching records immediately after subscribing, it hangs forever.
@@ -98,31 +138,34 @@ def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='
         records = [next(consumer)]
     except StopIteration:
         # No messages in this topic at all.
-        records = []
-    else:
-        # Ask what the most recent message's "offset" is,
-        # so we'll know for sure that we downloaded the whole log.
-        topic_partition = next(iter(consumer.assignment()))
-        end_offset = consumer.end_offsets([topic_partition])[topic_partition]
-    
-        logger.info(f"Reading kafka messages from {kafka_servers} for {server} / {uuid} / {instance}")
-        with Timer() as timer:
-            # Read all messages (until consumer timeout)
-            records += list(consumer)
-        logger.info(f"Reading {len(records)} kafka messages took {timer.seconds} seconds")
-    
-        # If there is an unexpected delay (e.g. a weird network/server hiccup),
-        # The log may be truncated.  Raise an error in that case.
-        if records[-1].offset < (end_offset-1):
-            msg = (f"Kafka log appears incomplete: \n"
-                   f"Expected to log to end with offset of >= {end_offset-1}, but last "
-                   f"message has offset of only {records[-1].offset}")
-            raise RuntimeError(msg)
+        return []
 
-    # Extract and parse JSON values for easy filtering.
-    values = [json.loads(rec.value) for rec in records]
-    records_and_values = zip(records, values)
+    # Ask what the most recent message's "offset" is,
+    # so we'll know for sure that we downloaded the whole log.
+    topic_partition = next(iter(consumer.assignment()))
+    end_offset = consumer.end_offsets([topic_partition])[topic_partition]
 
+    # Read all messages (until consumer timeout)
+    records += list(consumer)
+
+    # If there is an unexpected delay (e.g. a weird network/server hiccup),
+    # The log may be truncated.  Raise an error in that case.
+    if records[-1].offset < (end_offset-1):
+        msg = (f"Kafka log appears incomplete: \n"
+               f"Expected to log to end with offset of >= {end_offset-1}, but last "
+               f"message has offset of only {records[-1].offset}")
+        raise RuntimeError(msg)
+
+    return records
+
+
+def _filter_records_for_dag(records_and_values, dag_filter, server, repo_uuid, uuid, session):
+    """
+    Helper function.
+    Filter the given zipped records and JSON values according to the dag_filter.
+    Returns an iterator.
+    """
+    assert dag_filter in ('leaf-only', 'leaf-and-parents', None)
     if dag_filter == 'leaf-only':
         records_and_values = filter(lambda r_v: uuids_match(r_v[1]["UUID"], uuid), records_and_values)
 
@@ -142,24 +185,20 @@ def read_kafka_messages(server, uuid, instance, action_filter=None, dag_filter='
     else:
         assert dag_filter is None, f"Invalid choice for dag_filter: {dag_filter}"
 
-    if action_filter is not None:
-        if isinstance(action_filter, str):
-            action_filter = [action_filter]
-        action_filter = set(action_filter)
-        records_and_values = filter(lambda r_v: r_v[1]["Action"] in action_filter, records_and_values)
-    
-    # Evaluate filter
-    records_and_values = list(records_and_values)
-    
-    # Unzip
-    if records_and_values:
-        records, values = zip(*records_and_values)
-    else:
-        records, values = [], []
+    return records_and_values
 
-    if return_format == 'records':
-        return records
-    elif return_format == 'json-values':
-        return values
-    else:
-        raise AssertionError(f"Invalid return_format: {return_format}")
+
+def _filter_records_for_action(records_and_values, action_filter):
+    """
+    Helper function.
+    Filter the given zipped records and JSON values according to the action_filter.
+    Simply select records whose actions are in the given set.
+    Returns an iterator.
+    """
+    if action_filter is None:
+        return records_and_values
+
+    if isinstance(action_filter, str):
+        action_filter = [action_filter]
+    action_filter = set(action_filter)
+    return filter(lambda r_v: r_v[1]["Action"] in action_filter, records_and_values)
