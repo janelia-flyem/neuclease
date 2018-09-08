663a8c432795462d0525fc8abdadce94fc7c5270
Stuart Berg <bergs@janelia.hhmi.org>
2018-09-07 21:07:53 -0400

    check_tarsupervoxels_status: Don't fetch sizes

--- neuclease/misc/check_tarsupervoxels_status.py
+++ neuclease/misc/check_tarsupervoxels_status.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from neuclease import configure_default_logging
 from neuclease.util import read_csv_col, tqdm_proxy
-from neuclease.dvid import fetch_missing, fetch_exists, fetch_sizes
+from neuclease.dvid import fetch_missing, fetch_exists
 from neuclease.dvid.labelmap._labelmap import fetch_complete_mappings
 
 logger = logging.getLogger(__name__)
@@ -17,7 +17,7 @@ def main():
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--use-mapping', action='store_true')
-    parser.add_argument('--output', '-o', default='tsv-status.csv')
+    parser.add_argument('--output', '-o', default='missing-from-tsv.csv')
     parser.add_argument('server')
     parser.add_argument('uuid')
     parser.add_argument('seg_instance')
@@ -27,27 +27,26 @@ def main():
     
     bodies = read_csv_col(args.bodies_csv, 0, np.uint64)
     if args.use_mapping:
-        status_df = check_tarsupervoxels_status_via_exists(args.server, args.uuid, args.tsv_instance, args.seg_instance, bodies)
+        missing_entries = check_tarsupervoxels_status_via_exists(args.server, args.uuid, args.tsv_instance, args.seg_instance, bodies)
     else:
-        status_df = check_tarsupervoxels_status_via_missing(args.server, args.uuid, args.tsv_instance, args.seg_instance, bodies)
+        missing_entries = check_tarsupervoxels_status_via_missing(args.server, args.uuid, args.tsv_instance, args.seg_instance, bodies)
     
     logger.info(f"Writing to {args.output}")
-    status_df.to_csv(args.output, index=True, header=True)
+    missing_entries.to_csv(args.output, index=True, header=True)
     logging.info("DONE")
 
 
 def check_tarsupervoxels_status_via_missing(server, uuid, tsv_instance, seg_instance, bodies):
     """
     For the given bodies, query the given tarsupervoxels instance and return a
-    DataFrame indicating which supervoxels are 'missing' from the instance,
-    along with their sizes.
+    DataFrame indicating which supervoxels are 'missing' from the instance.
     
     Bodies that no longer exist in the segmentation instance are ignored.
 
     This function uses the /missing endpoint, which incurs a disk read in DVID 
     for the LabelIndex of each body.
     """
-    body_sv_sizes = []
+    sv_body = []
     
     try:
         for body in tqdm_proxy(bodies):
@@ -59,25 +58,20 @@ def check_tarsupervoxels_status_via_missing(server, uuid, tsv_instance, seg_inst
                 else:
                     raise
             
-            if len(missing_svs) == 0:
-                continue
-
-            sizes = fetch_sizes(server, uuid, seg_instance, missing_svs, supervoxels=True)
-            body_sv_sizes += [(body, sv, size) for (sv, size) in zip(missing_svs, sizes)]
+            sv_body += [(sv, body) for sv in missing_svs]
 
     except KeyboardInterrupt:
         logger.warning("Interrupted. Returning results so far.  Interrupt again to kill.")
 
-    df = pd.DataFrame(body_sv_sizes, columns=['body', 'sv', 'voxel_count'], dtype=np.uint64)
+    df = pd.DataFrame(sv_body, columns=['sv', 'body'], dtype=np.uint64)
     df.set_index('sv', inplace=True)
-    return df
+    return df['body']
 
 
 def check_tarsupervoxels_status_via_exists(server, uuid, tsv_instance, seg_instance, bodies):
     """
     For the given bodies, query the given tarsupervoxels instance and return a
-    DataFrame indicating which supervoxels are 'missing' from the instance,
-    along with their sizes.
+    DataFrame indicating which supervoxels are 'missing' from the instance.
     
     Bodies that no longer exist in the segmentation instance are ignored.
 
@@ -86,16 +80,17 @@ def check_tarsupervoxels_status_via_exists(server, uuid, tsv_instance, seg_insta
     query for missing supervoxels, rather than /missing, which incurs a disk
     read in DVID.
     """
-    body_sv_sizes = []
+    sv_body = []
     
     try:
         mapping = fetch_complete_mappings(server, uuid, seg_instance)
         
+        # Filter out bodies we don't care about,
+        # and append unmapped (singleton/identity) bodies
         _bodies = set(bodies)
         mapping = pd.DataFrame(mapping).query('body in @_bodies')['body'].copy()
         unmapped_bodies = _bodies - set(mapping)
         unmapped_bodies = np.fromiter(unmapped_bodies, np.uint64)
-
         singleton_mapping = pd.Series(index=unmapped_bodies, data=unmapped_bodies)
         mapping = pd.concat((mapping, singleton_mapping))
 
@@ -109,15 +104,14 @@ def check_tarsupervoxels_status_via_exists(server, uuid, tsv_instance, seg_insta
                 continue
 
             missing_bodies = mapping.loc[missing_svs]
-            sizes = fetch_sizes(server, uuid, seg_instance, missing_svs, supervoxels=True)
-            body_sv_sizes += list(zip(missing_bodies, missing_svs, sizes))
+            sv_body += list(zip(missing_svs, missing_bodies))
         
     except KeyboardInterrupt:
         logger.warning("Interrupted. Returning results so far.  Interrupt again to kill.")
 
-    df = pd.DataFrame(body_sv_sizes, columns=['body', 'sv', 'voxel_count'], dtype=np.uint64)
+    df = pd.DataFrame(sv_body, columns=['sv', 'body'], dtype=np.uint64)
     df.set_index('sv', inplace=True)
-    return df
+    return df['body']
 
 
 ###
