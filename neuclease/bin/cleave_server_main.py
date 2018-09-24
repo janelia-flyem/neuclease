249a6870e4219b9d8226c1e6fdccd832811b5de6
Stuart Berg <bergs@janelia.hhmi.org>
2018-09-24 13:36:36 -0400

    cleave_server_main: Declare a main function

--- neuclease/bin/cleave_server_main.py
+++ neuclease/bin/cleave_server_main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 import neuclease.cleave_server
 
-if __name__ == "__main__":
+# main function declared so this file can be used as console_script in setup.py
+def main():
     neuclease.cleave_server.main(debug_mode=False)
+
+if __name__ == "__main__":
+    main()
