06729048957226b6761c860a5a2c03429bd53f31
Stuart Berg <bergs@janelia.hhmi.org>
2018-09-24 13:30:07 -0400

    flyem-production-commands

--- flyem-production-commands.sh
+++ flyem-production-commands.sh
@@ -7,16 +7,17 @@
 ACTIVATE=/groups/flyem/proj/cluster/miniforge/bin/activate
 source $ACTIVATE flyem
 
-# emdata3:8900 -- d5852c27b5c04687bb1be414f6dc2336
-# Run this on emdata3 to produce a cleave server on emdata3:5551
+
+# Using emdata3:8900 as the primary node, cleave server hosted on emdata1
+# Run this on emdata1 to produce a cleave server on emdata1:5551
 
 CLEAVE_PORT=5551
 nohup neuclease_cleave_server \
   -p ${CLEAVE_PORT} \
-  --log-dir /nrs/flyem/bergs/neuclease-logs/production-emdata3-8900-${CLEAVE_PORT} \
+  --log-dir /nrs/flyem/bergs/neuclease-logs/production-emdata3-8900-emdata1-${CLEAVE_PORT} \
   --merge-table /nrs/flyem/bergs/final-agglo-fixsplit-patched/final_patched_20180426_merge_table.npy \
   --primary-dvid-server emdata3:8900 \
-  --primary-uuid 07160ccb9ee849ad8465c3b617bb90e5 \
+  --primary-uuid 7f0cf466136b4d56aeaeffe4a494c6ab \
   --primary-labelmap-instance segmentation \
   &
 ##
