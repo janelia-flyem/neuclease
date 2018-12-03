aa65e8d27a5e37827d2e915199e847b6dd2ccb91
Stuart Berg <bergs@janelia.hhmi.org>
2018-12-02 21:04:00 -0500

    Added dvid.server.reload_metadata()

--- neuclease/dvid/server.py
+++ neuclease/dvid/server.py
@@ -1,7 +1,14 @@
 from . import dvid_api_wrapper, fetch_generic_json
 
+
 @dvid_api_wrapper
 def fetch_server_info(server, *, session=None):
     return fetch_generic_json(f'http://{server}/api/server/info', session=session)
+
+
+@dvid_api_wrapper
+def reload_metadata(server, *, session=None):
+    r = session.post(f"http://{server}/api/server/reload-metadata")
+    r.raise_for_status()
 
 
