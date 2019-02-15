1b33a4d4fd41ffddf829e0cc4ab0c088aab714b8
Stuart Berg <bergs@janelia.hhmi.org>
2019-02-15 15:17:47 -0500

    Added segmentation.split_disconnected_bodies() (Copied from DVIDSparkServices)

---
+++ neuclease/tests/test_segmentation.py
@@ -0,0 +1,97 @@
+from itertools import product
+
+import pytest
+import numpy as np
+import pandas as pd
+
+from dvidutils import LabelMapper
+from neuclease.util import contingency_table, split_disconnected_bodies
+
+def test_contingency_table_simple():
+    left =  np.array([[0,0,0,1,1,1,2,2,2,3,3,3,4]])
+    right = np.array([[0,0,5,5,5,6,6,6,7,7,7,8,0]])
+    
+    table = contingency_table(left, right)
+    assert isinstance(table, pd.Series)
+    assert (np.array(table.index.values.tolist()) == [(0,0), (0,5), (1,5), (1,6), (2,6), (2,7), (3,7), (3,8), (4,0)]).all()
+    assert (table == [2,1,2,1,2,1,2,1,1]).all()
+
+
+def test_contingency_table_random():
+    a = np.random.randint(5,10, size=(20,20), dtype=np.uint32)
+    b = np.random.randint(10,15, size=(20,20), dtype=np.uint32)
+    table = contingency_table(a,b)
+
+    for (val_a, val_b) in product(range(5,10), range(10,15)):
+        expected_overlap = ((a == val_a) & (b == val_b)).sum()
+        rows = pd.DataFrame(table).query('left == @val_a and right == @val_b')
+        if expected_overlap == 0:
+            assert len(rows) == 0
+        else:
+            assert len(rows) == 1
+            assert rows['voxel_count'].iloc[0] == expected_overlap
+
+
+def test_split_disconnected_bodies():
+
+    _ = 2 # for readability in the array below
+
+    # Note that we multiply these by 10 for this test!
+    orig = [[ 1,1,1,1,_,_,3,3,3,3 ],
+            [ 1,1,1,1,_,_,3,3,3,3 ],
+            [ 1,1,1,1,_,_,3,3,3,3 ],
+            [ 1,1,1,1,_,_,3,3,3,3 ],
+            [ _,_,_,_,_,_,_,_,_,_ ],
+            [ 0,0,_,_,4,4,_,_,0,0 ],  # Note that the zeros here will not be touched.
+            [ _,_,_,_,4,4,_,_,_,_ ],
+            [ 1,1,1,_,_,_,_,3,3,3 ],
+            [ 1,1,1,_,1,1,_,3,3,3 ],
+            [ 1,1,1,_,1,1,_,3,3,3 ]]
+    
+    orig = np.array(orig).astype(np.uint64)
+    orig *= 10
+    
+    split, mapping = split_disconnected_bodies(orig)
+    
+    # Due to undocumented requirements in Evaluate.py, the mapping
+    # returned by split_disconnected_bodies() must contain plain Python integers,
+    # not np.uint64, etc.
+    assert all(type(k) == int and type(v) == int for k,v in mapping.items()), \
+        "Returned mapping does not contain plain Python int types"
+    
+    assert ((orig == 20) == (split == 20)).all(), \
+        "Label 2 is a single component and therefore should remain untouched in the output"
+
+    assert ((orig == 40) == (split == 40)).all(), \
+        "Label 4 is a single component and therefore should remain untouched in the output"
+    
+    assert (split[:4,:4] == 10).all(), \
+        "The largest segment in each split object is supposed to keep it's label"
+    assert (split[:4,-4:] == 30).all(), \
+        "The largest segment in each split object is supposed to keep it's label"
+
+    lower_left_label = split[-1,1]
+    lower_right_label = split[-1,-1]
+    bottom_center_label = split[-1,5]
+
+    assert lower_left_label != 10, "Split object was not relabeled"
+    assert bottom_center_label != 10, "Split object was not relabeled"
+    assert lower_right_label != 30, "Split object was not relabeled"
+
+    assert lower_left_label in (41,42,43), "New labels are supposed to be consecutive with the old"
+    assert lower_right_label in (41,42,43), "New labels are supposed to be consecutive with the old"
+    assert bottom_center_label in (41,42,43), "New labels are supposed to be consecutive with the old"
+
+    assert (split[-3:,:3] == lower_left_label).all()
+    assert (split[-3:,-3:] == lower_right_label).all()
+    assert (split[-2:,4:6] == bottom_center_label).all()
+
+    assert set(mapping.keys()) == set([10,30,41,42,43]), "mapping: {}".format( mapping )
+
+    mapper = LabelMapper(np.fromiter(mapping.keys(), np.uint64), np.fromiter(mapping.values(), np.uint64))
+    assert (mapper.apply(split, True) == orig).all(), \
+        "Applying mapping to the relabeled image did not recreate the original image."
+
+
+if __name__ == "__main__":
+    pytest.main(['-s', '--tb=native', '--pyargs', 'neuclease.tests.test_segmentation'])
--- neuclease/util/segmentation.py
+++ neuclease/util/segmentation.py
@@ -1,7 +1,11 @@
+from collections import OrderedDict
+
 import vigra
 import numpy as np
 import pandas as pd
-from collections import OrderedDict
+import skimage.measure as skm
+
+from dvidutils import LabelMapper
 
 from . import Grid, boxes_from_grid, box_intersection, box_to_slicing
 
@@ -117,4 +121,75 @@ def compute_cc(img, min_component=1):
     cc_mapping = cc_mapping_df.set_index('cc')['orig']
     return img_cc, cc_mapping
     
+
+def split_disconnected_bodies(labels_orig):
+    """
+    Produces 3D volume split into connected components.
+
+    This function identifies bodies that are the same label
+    but are not connected.  It splits these bodies and
+    produces a dict that maps these newly split bodies to
+    the original body label.
+
+    Special exception: Segments with label 0 are not relabeled.
+
+    Args:
+        labels_orig (numpy.array): 3D array of labels
+
+    Returns:
+        (labels_new, new_to_orig)
+
+        labels_new:
+            The partially relabeled array.
+            Segments that were not split will keep their original IDs.
+            Among split segments, the largest 'child' of a split segment retains the original ID.
+            The smaller segments are assigned new labels in the range (N+1)..(N+1+S) where N is
+            highest original label and S is the number of new segments after splitting.
+        
+        new_to_orig:
+            A pseudo-minimal (but not quite minimal) mapping of labels
+            (N+1)..(N+1+S) -> some subset of (1..N),
+            which maps new segment IDs to the segments they came from.
+            Segments that were not split at all are not mentioned in this mapping,
+            for split segments, every mapping pair for the split is returned, including the k->k (identity) pair.
+    """
+    # Compute connected components and cast back to original dtype
+    labels_cc = skm.label(labels_orig, background=0, connectivity=1)
+    assert labels_cc.dtype == np.int64
+    if labels_orig.dtype == np.uint64:
+        labels_cc = labels_cc.view(np.uint64)
+    else:
+        labels_cc = labels_cc.astype(labels_orig.dtype, copy=False)
+
+    # Find overlapping segments between orig and CC volumes
+    overlap_table_df = contingency_table(labels_orig, labels_cc).reset_index()
+    assert overlap_table_df.columns.tolist() == ['left', 'right', 'voxel_count']
+    overlap_table_df.columns = ['orig', 'cc', 'voxels']
+    overlap_table_df.sort_values('voxels', ascending=False, inplace=True)
+    
+    # If a label in 'orig' is duplicated, it has multiple components in labels_cc.
+    # The largest component gets to keep the original ID;
+    # the other components must take on new values.
+    # (The new values must not conflict with any of the IDs in the original, so start at orig_max+1)
+    new_cc_pos = overlap_table_df['orig'].duplicated()
+    orig_max = overlap_table_df['orig'].max()
+    new_cc_values = np.arange(orig_max+1, orig_max+1+new_cc_pos.sum(), dtype=labels_orig.dtype)
+
+    overlap_table_df['final_cc'] = overlap_table_df['orig'].copy()
+    overlap_table_df.loc[new_cc_pos, 'final_cc'] = new_cc_values
+    
+    # Relabel the CC volume to use the 'final_cc' labels
+    mapper = LabelMapper(overlap_table_df['cc'].values, overlap_table_df['final_cc'].values)
+    mapper.apply_inplace(labels_cc)
+
+    # Generate the mapping that could (if desired) convert the new
+    # volume into the original one, as described in the docstring above.
+    emitted_mapping_rows = overlap_table_df['orig'].duplicated(keep=False)
+    emitted_mapping_pairs = overlap_table_df.loc[emitted_mapping_rows, ['final_cc', 'orig']].values
+
+    # Use tolist() to ensure plain Python int types
+    # (This is required by some client code in Evaluate.py)
+    new_to_orig = dict(emitted_mapping_pairs.tolist())
+    
+    return labels_cc, new_to_orig
 
