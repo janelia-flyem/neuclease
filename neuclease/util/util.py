af9fa704972dec37ac129ce63715b8b29e8837e0
Stuart Berg <bergs@janelia.hhmi.org>
2019-07-15 12:05:10 -0400

    extract_labels_from_volume(): Allow floating-point coordinates

--- neuclease/util/util.py
+++ neuclease/util/util.py
@@ -591,7 +591,7 @@ def extract_labels_from_volume(points_df, volume, box_zyx=None, vol_scale=0, lab
 
     assert ((box_zyx[1] - box_zyx[0]) == volume.shape).all() 
 
-    downsampled_coords_zyx = points_df[['z', 'y', 'x']] // (2**vol_scale)
+    downsampled_coords_zyx = (points_df[['z', 'y', 'x']] // (2**vol_scale)).astype(np.int32)
 
     # Drop everything outside the combined_box
     min_z, min_y, min_x = box_zyx[0] #@UnusedVariable
