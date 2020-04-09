1228639844f75866b537d4f3639d721ebdc77562
Stuart Berg <bergs@janelia.hhmi.org>
2020-04-09 15:48:31 -0400

    generate_sample_coordinate(): Slightly better coordinate (middle coordinate of middle block)

--- neuclease/dvid/labelmap/_labelmap.py
+++ neuclease/dvid/labelmap/_labelmap.py
@@ -1192,7 +1192,7 @@ def generate_sample_coordinate(server, uuid, instance, label_id, supervoxels=Fal
     Returns:
         [Z,Y,X] -- An arbitrary point within the body of interest.
     """
-    SCALE = 6 # sparsevol-coarse is always scale 6
+    SCALE = 6  # sparsevol-coarse is always scale 6
     coarse_block_coords = fetch_sparsevol_coarse(server, uuid, instance, label_id, supervoxels, session=session)
     num_blocks = len(coarse_block_coords)
     middle_block_coord = (2**SCALE) * np.array(coarse_block_coords[num_blocks//2]) // 64 * 64
@@ -1205,7 +1205,7 @@ def generate_sample_coordinate(server, uuid, instance, label_id, supervoxels=Fal
         raise RuntimeError(f"The sparsevol-coarse info for this {label_type} ({label_id}) "
                            "appears to be out-of-sync with the scale-0 segmentation.")
 
-    return middle_block_coord + nonzero_coords[0]
+    return middle_block_coord + nonzero_coords[len(nonzero_coords)//2]
 
 
 @dvid_api_wrapper
