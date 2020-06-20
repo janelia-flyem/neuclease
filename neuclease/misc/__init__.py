53eb9b207d93111560ccb858fdfe796e7b097f13
Stuart Berg <bergs@janelia.hhmi.org>
2020-06-20 10:18:07 -0400

    Remove imports from misc package init

--- neuclease/misc/__init__.py
+++ neuclease/misc/__init__.py
@@ -4,5 +4,4 @@ Miscellaneous functions.
 Functions in this subpackage are not part of neuclease's public API,
 and may be unceremoniously dropped or changed.
 """
-from .find_best_plane import find_best_plane
-from .export_roi import load_roi_label_volume, export_roi_slices, export_downsampled_grayscale
+# We don't import anything here, to keep script entrypoints fast.
