793b200518bfd22e05001f4da629162f4c377cf7
Stuart Berg <bergs@janelia.hhmi.org>
2020-08-18 01:30:44 -0400

    util.segmentation.fill_hull() and friends

--- neuclease/util/segmentation.py
+++ neuclease/util/segmentation.py
@@ -1,6 +1,7 @@
 import logging
 from collections import OrderedDict
 
+import scipy
 import vigra
 import numpy as np
 import pandas as pd
@@ -8,7 +9,7 @@ from numba import njit
 
 from dvidutils import LabelMapper
 
-from . import Timer, Grid, boxes_from_grid, box_intersection, box_to_slicing, extract_subvol
+from . import Timer, Grid, boxes_from_grid, box_intersection, box_to_slicing, extract_subvol, downsample_mask, upsample
 
 logger = logging.getLogger(__name__)
 
@@ -554,3 +555,67 @@ def split_disconnected_bodies(labels_orig):
     
     return labels_cc, new_to_orig, new_unique_labels
 
+
+def fill_hull(image):
+    """
+    Compute the convex hull of the given binary image and
+    return a mask of the filled hull.
+
+    Adapted from:
+    https://stackoverflow.com/a/46314485/162094
+
+    This version is slightly (~40%) faster for 3D volumes,
+    by being a little more stingy with RAM.
+    """
+    # (The variable names below assume 3D input,
+    # but this would still work in 4D, etc.)
+
+    assert (np.array(image.shape) <= np.iinfo(np.int16).max).all(), \
+        f"This function assumes your image is smaller than {2**15} in each dimension"
+
+    mask = np.zeros_like(image, dtype=bool)
+    points = np.argwhere(image).astype(np.int16)
+
+    # Restrict analysis to the bounding-box
+    box = (points.min(axis=0), 1+points.max(axis=0))
+    mask_view = mask[box_to_slicing(*box)]
+    points -= box[0]
+
+    _fill_hull(points, mask_view)
+    return mask
+
+
+def _fill_hull(points, mask):
+    """
+    Compute the convex hull of the given points
+    set and write it into the given mask.
+    """
+    hull = scipy.spatial.ConvexHull(points)
+    deln = scipy.spatial.Delaunay(points[hull.vertices])
+
+    # Instead of allocating a giant array for all indices in the volume,
+    # just iterate over the slices one at a time.
+    idx_2d = np.indices(mask.shape[1:], np.int16)
+    idx_2d = np.moveaxis(idx_2d, 0, -1)
+
+    idx_3d = np.zeros((*mask.shape[1:], mask.ndim), np.int16)
+    idx_3d[:, :, 1:] = idx_2d
+
+    for z in range(len(mask)):
+        idx_3d[:,:,0] = z
+
+        # find_simplex() returns -1 for points that don't fall within any simplex.
+        s = deln.find_simplex(idx_3d)
+        mask[z, (s != -1)] = 1
+
+
+def fill_hull_for_segment(label_img, segment_id=1):
+    all_edges = edge_mask(label_img, 'both', True)
+    seg_edges = np.where(label_img == segment_id, all_edges, False)
+    return fill_hull(seg_edges)
+
+
+def approximate_hull_for_segment(label_img, segment_id, downsample_factor=2):
+    mask = downsample_mask(label_img == segment_id, downsample_factor)
+    hull = fill_hull_for_segment(mask)
+    return upsample(hull, downsample_factor)
