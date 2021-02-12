84d4ae0773212583f2b083f274a9d5922a23cf58
Stuart Berg <bergs@janelia.hhmi.org>
2021-02-11 23:58:42 -0500

    copy_synapses: Now a reusable function

--- neuclease/misc/copy_synapses.py
+++ neuclease/misc/copy_synapses.py
@@ -23,6 +23,9 @@ Note:
 """
 import logging
 
+from functools import partial
+from collections import namedtuple
+
 from neuclease.util import boxes_from_grid, compute_parallel
 from neuclease.dvid import fetch_repo_instances, create_instance, fetch_instance_info, fetch_volume_box
 from neuclease.dvid.annotation import post_sync, fetch_blocks, post_blocks
@@ -32,50 +35,60 @@ configure_default_logging()
 
 logger = logging.getLogger(__name__)
 
-PROCESSES = 8
-
-# Source to copy from
-src_node = ('emdata4:8900', '52a13328874c4bb7b15dc4280da26576')
-src_syn = 'synapses'
-src_seg = 'segmentation' # Needed to determine the bounding box of all synapses to fetch
-
-# Destination to copy to, and the segmentation instance to sync to.
-dst_node = ('emdata2:7900', '23400dc115d84f0f92db068c7ab84b65')
-dst_syn = 'upload_production_synapses_2020-01-03'
-dst_seg = 'segmentation'
+Location = namedtuple('Location', 'server uuid syn_instance seg_instance')
 
 
-def copy_syn_blocks(box):
-    blocks = fetch_blocks(*src_node, src_syn, box)
+def copy_syn_blocks(src_loc, dst_loc, box):
+    blocks = fetch_blocks(*src_loc[:3], box)
     if blocks:
-        post_blocks(*dst_node, dst_syn, blocks)
+        post_blocks(*dst_loc[:3], blocks)
 
-def main():
+
+def copy_synapses(src_loc, dst_loc, processes):
+    """
+    See caveats in the module docstring above.
+    """
+    src_loc = Location(*src_loc)
+    dst_loc = Location(*dst_loc)
+
     # Create the destination instance if necessary.
-    dst_instances = fetch_repo_instances(*dst_node, 'annotation')
-    if dst_syn not in dst_instances:
-        logger.info(f"Creating instance '{dst_syn}'")
-        create_instance(*dst_node, dst_syn, 'annotation')
+    dst_instances = fetch_repo_instances(*dst_loc[:2], 'annotation')
+    if dst_loc.syn_instance not in dst_instances:
+        logger.info(f"Creating instance '{dst_loc.syn_instance}'")
+        create_instance(*dst_loc, 'annotation')
 
     # Check to see if the sync already exists; add it if necessary
-    syn_info = fetch_instance_info(*dst_node, dst_syn)
+    syn_info = fetch_instance_info(*dst_loc[:3])
     if len(syn_info["Base"]["Syncs"]) == 0:
-        logger.info(f"Adding a sync to '{dst_syn}' from '{dst_seg}'")
-        post_sync(*dst_node, dst_syn, [dst_seg])
-    elif syn_info["Base"]["Syncs"][0] != dst_seg:
+        logger.info(f"Adding a sync to '{dst_loc.syn_instance}' from '{dst_loc.seg_instance}'")
+        post_sync(*dst_loc[:3], [dst_loc.seg_instance])
+    elif syn_info["Base"]["Syncs"][0] != dst_loc.seg_instance:
         other_seg = syn_info["Base"]["Syncs"][0]
-        raise RuntimeError(f"Can't create a sync to '{dst_seg}'. "
+        raise RuntimeError(f"Can't create a sync to '{dst_loc.seg_instance}'. "
                            f"Your instance is already sync'd to a different segmentation: {other_seg}")
 
     # Fetch segmentation extents
-    bounding_box_zyx = fetch_volume_box(*src_node, src_seg).tolist()
+    bounding_box_zyx = fetch_volume_box(*src_loc[:2], src_loc.seg_instance).tolist()
 
     # Break into block-aligned chunks (boxes) that are long in the X direction
     # (optimal access pattern for dvid read/write)
     boxes = boxes_from_grid(bounding_box_zyx, (256,256,6400), clipped=True)
-    
+
     # Use a process pool to copy the chunks in parallel.
-    compute_parallel(copy_syn_blocks, boxes, processes=PROCESSES, ordered=False)
+    fn = partial(copy_syn_blocks, src_loc, dst_loc)
+    compute_parallel(fn, boxes, processes=processes, ordered=False)
+
+
+def main():
+    # Source to copy from
+    src_loc = Location('emdata4:8900', '52a13328874c4bb7b15dc4280da26576', 'synapses', 'segmentation')
+
+    # Destination to copy to, and the segmentation instance to sync to.
+    dst_loc = Location('emdata2:7900', '23400dc115d84f0f92db068c7ab84b65', 'upload_production_synapses_2020-01-03', 'segmentation')
+
+    PROCESSES = 8
+    copy_synapses(src_loc, dst_loc, PROCESSES)
+
 
 if __name__ == "__main__":
     main()
