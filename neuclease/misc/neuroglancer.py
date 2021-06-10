64108aedf9d6313881b3d35c189a4c7303117d53
Stuart Berg <bergs@janelia.hhmi.org>
2021-06-09 23:08:59 -0400

    Added misc.neuroglancer.extract_annotations()

--- neuclease/misc/neuroglancer.py
+++ neuclease/misc/neuroglancer.py
@@ -3,7 +3,49 @@ neuroglancer-related utility functions
 """
 
 import copy
+import json
+import urllib
 import numpy as np
+import pandas as pd
+from textwrap import dedent
+
+
+def parse_nglink(link):
+    url_base, pseudo_json = link.split('#!')
+    pseudo_json = urllib.parse.unquote(pseudo_json)
+    data = json.loads(pseudo_json)
+    return data
+
+
+def extract_annotations(link, link_index=None, user=None):
+    if isinstance(link, str):
+        link = parse_nglink(link)
+    annotation_layer = [layer for layer in link['layers'] if layer['type'] == "annotation"][0]
+    data = []
+    for a in annotation_layer['annotations']:
+        data.append((*a['point'], a['description']))
+
+    df = pd.DataFrame(data, columns=[*'xyz', 'description'])
+
+    cols = []
+    if link_index is not None:
+        df['link_index'] = link_index
+        cols += ['link_index']
+    if user is not None:
+        df['user'] = user
+        cols += ['user']
+
+    df = df.astype({k: np.int64 for k in 'xyz'})
+    cols += [*'xyz', 'description']
+    return df[cols]
+
+
+SHADER_FMT = dedent("""\
+    void main() {{
+        setColor(defaultColor());
+        setPointMarkerSize({size:.1f});
+    }}
+""")
 
 LOCAL_ANNOTATION_JSON = {
     "name": "annotations",
@@ -28,6 +70,7 @@ LOCAL_ANNOTATION_JSON = {
         }
     },
     "tool": "annotatePoint",
+    "shader": "\nvoid main() {\n  setColor(defaultColor());\n  setPointMarkerSize(8.0);\n}\n",
     "panels": [
         {
             "row": 1,
@@ -46,11 +89,17 @@ LOCAL_ANNOTATION_JSON = {
 }
 
 
-def point_annotation_layer_json(points_df):
+def point_annotation_layer_json(points_df, name="annotations", color="#ffff00", size=8.0):
     """
     Construct the JSON data for a neuroglancer local point annotations layer.
     This does not result in a complete neuroglancer link; it results in something
     that can be added to the layers list in the neuroglancer viewer JSON state.
+
+    Args:
+        points_df:
+            DataFrame with columns ['x', 'y', 'z'] and optionally 'id' and 'description'.
+    Returns:
+        dict (JSON data)
     """
     assert {*'xyz'} <= set(points_df.columns), 'x,y,z are required columns'
     points_df = points_df.copy()
@@ -61,10 +110,14 @@ def point_annotation_layer_json(points_df):
         points_df['id'] = [*map(str, ids)]
 
     data = copy.deepcopy(LOCAL_ANNOTATION_JSON)
+    data['name'] = name
+    data['annotationColor'] = color
+    data['shader'] = SHADER_FMT.format(size=size)
     data['annotations'].clear()
 
     for row in points_df.itertuples():
         entry = {}
+        entry['type'] = "point"
         entry['point'] = [row.x, row.y, row.z]
         entry['id'] = row.id
         if 'description' in points_df.columns:
@@ -72,3 +125,5 @@ def point_annotation_layer_json(points_df):
         data['annotations'].append(entry)
 
     return data
+
+
