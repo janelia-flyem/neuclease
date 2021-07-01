janelia-flyem/neuclease

commit 45911f54de17254d6fd230a0104d6154245e68fd

Upgrade to pandas 1.x

neuclease/merge_graph.py
@@ -144,8 +144,9 @@ class LabelmapMergeGraph:
         for col, dtype in MERGE_TABLE_DTYPE:
             focused_merges[col] = focused_merges[col].astype(dtype, copy=False)
         
-        focused_merges = focused_merges.loc[:, list(self.merge_table_df.columns)]
-        self.merge_table_df = pd.concat((self.merge_table_df, focused_merges), ignore_index=True, copy=False)
+        cols = self.merge_table_df.columns.intersection(focused_merges.columns).tolist()
+        focused_merges = focused_merges[cols]
+        self.merge_table_df = pd.concat((self.merge_table_df, focused_merges), sort=True, ignore_index=True, copy=False)
         return len(focused_merges)
 
 

conda-recipe/meta.yaml
@@ -30,7 +30,7 @@ requirements:
     - scipy
     - scikit-image
     - h5py
-    - pandas <1.0
+    - pandas
     - numba >=0.45
     - pytz
     - vigra

commit b559f88041900032b82ba116f3b17ea3c8c13e14

merge_graph: Fix wrong column order  merge_table_df, introduced in 45911f54de17254d6fd230a0104d6154245e68fd

neuclease/merge_graph.py
@@ -144,9 +144,8 @@ class LabelmapMergeGraph:
         for col, dtype in MERGE_TABLE_DTYPE:
             focused_merges[col] = focused_merges[col].astype(dtype, copy=False)
         
-        cols = self.merge_table_df.columns.intersection(focused_merges.columns).tolist()
-        focused_merges = focused_merges[cols]
-        self.merge_table_df = pd.concat((self.merge_table_df, focused_merges), sort=True, ignore_index=True, copy=False)
+        focused_merges = focused_merges.reindex(columns=self.merge_table_df.columns)
+        self.merge_table_df = pd.concat((self.merge_table_df, focused_merges), ignore_index=True, copy=False)
         return len(focused_merges)
 
 
@@ -241,7 +240,8 @@ class LabelmapMergeGraph:
                     update_rows.append(parent_rows_df[i:i+1])
 
         update_table_df = pd.concat(update_rows, ignore_index=True)
-        assert (update_table_df.columns == self.merge_table_df.columns).all()
+        assert (update_table_df.columns == self.merge_table_df.columns).all(), \
+            f"{update_table_df.columns.tolist()} != {self.merge_table_df.columns.tolist()}"
 
         if bad_edges:
             bad_edges = pd.DataFrame(bad_edges, columns=['end', 'found_sv'] + list(update_table_df.columns))