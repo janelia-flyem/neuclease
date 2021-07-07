f018cf8fd29569178467beec340181270ca9e8c4
Stuart Berg <bergs@janelia.hhmi.org>
2021-07-07 16:26:57 -0400

    Added util.fix_df_names()

--- neuclease/util/util.py
+++ neuclease/util/util.py
@@ -1070,6 +1070,13 @@ def drop_unordered_duplicates(df, subset=None, keep='first'):
     return df.loc[~dupes]
 
 
+def fix_df_names(df):
+    """
+    Rename all columns of the given dataframe with programmer-friendly alternatives,
+    i.e. lowercase and replace spaces with underscores.
+    """
+    return df.rename(columns={c: c.lower().replace(' ', '_') for c in df.columns})
+
 def swap_df_cols(df, prefixes=None, swap_rows=None, suffixes=['_a', '_b']):
     """
     Swap selected columns of a dataframe, specified as a list of prefixes and two suffixes.
