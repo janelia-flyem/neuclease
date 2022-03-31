07d5e4b8cc47ca759ef94c5f334ef030608e8d5a
Stuart Berg <bergs@janelia.hhmi.org>
2022-03-31 09:37:25 -0400

    Added clio.api.fetch_users()

--- neuclease/clio/api.py
+++ neuclease/clio/api.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 from . import clio_api_wrapper
 
+# Clio API Swagger docs can be found here:
+# https://clio-store-vwzoicitea-uk.a.run.app/docs
 
 @clio_api_wrapper
 def fetch_annotations(dataset, user=None, format='pandas', *, base=None, session=None):
@@ -31,3 +33,9 @@ def fetch_json_annotations_all(dataset, annotation_type='neurons', format='panda
 
     if format == 'pandas':
         return pd.DataFrame(r.json())
+
+@clio_api_wrapper
+def fetch_users(*, base=None, session=None):
+    r = session.get(f"{base}/v2/users")
+    r.raise_for_status()
+    return r.json()
