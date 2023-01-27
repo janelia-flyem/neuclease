b31b7ea89494d87f5d7134d5de665678af6ad627
Stuart Berg <bergs@janelia.hhmi.org>
2023-01-26 22:23:42 -0500

    clio.fetch_json_annotations_all(): Add parameters

--- neuclease/clio/api.py
+++ neuclease/clio/api.py
@@ -5,6 +5,7 @@ from . import clio_api_wrapper
 # Clio API Swagger docs can be found here:
 # https://clio-store-vwzoicitea-uk.a.run.app/docs
 
+
 @clio_api_wrapper
 def fetch_annotations(dataset, user=None, format='pandas', *, base=None, session=None):
     assert format in ('json', 'pandas')
@@ -22,17 +23,31 @@ def fetch_annotations(dataset, user=None, format='pandas', *, base=None, session
     if format == 'pandas':
         return pd.DataFrame(r.json())
 
-@clio_api_wrapper
-def fetch_json_annotations_all(dataset, annotation_type='neurons', format='pandas', *, base=None, session=None):
-    assert format in ('json', 'pandas')
-    r = session.get(f"{base}/v2/json-annotations/{dataset}/{annotation_type}/all")
-    r.raise_for_status()
 
-    if format == 'json':
-        return r.json()
+@clio_api_wrapper
+def fetch_json_annotations_all(dataset, annotation_type='neurons', *, fields=None, show=None, format='pandas', base=None, session=None):
+    assert fields is None, "At this time, the 'fields' argument is not supported by the clio service.  (DVID does support it, though.)"
+    assert show in ('user', 'time', 'all', None)
+    assert format in ('pandas', 'json')
+
+    params = {}
+    if show:
+        params['show'] = show
+
+    if fields:
+        if isinstance(fields, str):
+            fields = [fields]
+        params['fields'] = ','.join(fields)
+
+    r = session.get(f"{base}/v2/json-annotations/{dataset}/{annotation_type}/all", params=params)
+    r.raise_for_status()
+    values = r.json() or []
 
     if format == 'pandas':
-        return pd.DataFrame(r.json())
+        return pd.DataFrame(values)
+    else:
+        return sorted(values, key=lambda d: d['bodyid'])
+
 
 @clio_api_wrapper
 def fetch_users(*, base=None, session=None):
