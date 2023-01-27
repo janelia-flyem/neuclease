8450a6724163f84cba86c21845b7418cd42880ee
Stuart Berg <bergs@janelia.hhmi.org>
2023-01-26 22:25:04 -0500

    neuronjson.fetch_all(): Add support for new optional parameter 'fields'

--- neuclease/dvid/neuronjson.py
+++ neuclease/dvid/neuronjson.py
@@ -1,16 +1,27 @@
-from . import dvid_api_wrapper, fetch_generic_json
+from . import dvid_api_wrapper
 from .keyvalue._keyvalue import _body_annotations_dataframe
 
 
 @dvid_api_wrapper
-def fetch_all(server, uuid, instance='segmentation_annotations', show=None, *, status_categories=None, format='pandas', session=None):
-    params = None
+def fetch_all(server, uuid, instance='segmentation_annotations', *, show=None, fields=None, status_categories=None, format='pandas', session=None):
     assert show in ('user', 'time', 'all', None)
+    assert format in ('pandas', 'json')
+
+    params = {}
     if show:
-        params = {'show': show}
+        params['show'] = show
+
+    if fields:
+        if isinstance(fields, str):
+            fields = [fields]
+        params['fields'] = ','.join(fields)
 
     url = f'{server}/api/node/{uuid}/{instance}/all'
-    values = fetch_generic_json(url, params=params, session=session)
-    return _body_annotations_dataframe(values, status_categories)
+    r = session.get(url, params=params)
+    r.raise_for_status()
+    values = r.json() or []
 
-
+    if format == 'pandas':
+        return _body_annotations_dataframe(values, status_categories)
+    else:
+        return sorted(values, key=lambda d: d['bodyid'])
