afcca41b891c4a1c3dd2d62bbd92c8595fb58287
Stuart Berg <bergs@janelia.hhmi.org>
2023-08-08 18:08:27 -0400

    iter_batches: Handle batch_size=0 with an error or not, depending on the length of the iterator.

--- neuclease/util/util.py
+++ neuclease/util/util.py
@@ -548,15 +548,18 @@ def _gen_json_objects(text_array):
 def iter_batches(it, batch_size):
     """
     Iterator.
-    
+
     Consume the given iterator/iterable in batches and
     yield each batch as a list of items.
-    
+
     The last batch might be smaller than the others,
     if there aren't enough items to fill it.
-    
+
     If the given iterator supports the __len__ method,
     the returned batch iterator will, too.
+
+    This iterator implements special (efficient) support
+    for lists, numpy arrays, Series/DataFrames, and h5py.Datasets.
     """
     if hasattr(it, '__len__'):
         return _iter_batches_with_len(it, batch_size)
@@ -566,38 +569,61 @@ def iter_batches(it, batch_size):
 
 class _iter_batches:
     def __init__(self, it, batch_size):
+        assert batch_size >= 0
         self.base_iterator = it
-        self.batch_size = batch_size
-                
+        self.batch_size = self._validate_batch_size(it, batch_size)
+
+    @classmethod
+    def _validate_batch_size(cls, it, batch_size):
+        """
+        User is not allowed to provide a batch_size of 0 unless
+        the iterable is empty anyway.
+        """
+        if batch_size > 0:
+            return batch_size
+
+        msg = "Can't use batch_size of 0 with a non-empty iterable"
+        if hasattr(it, '__len__'):
+            assert len(it) == 0, msg
+            return 1
+
+        try:
+            next(it)
+        except StopIteration:
+            # Iterable was empty, so we won't
+            # complain about the zero batch size.
+            return 1
+
+        raise RuntimeError(msg)
 
     def __iter__(self):
         return self._iter_batches(self.base_iterator, self.batch_size)
-    
 
     def _iter_batches(self, it, batch_size):
         if isinstance(it, (pd.DataFrame, pd.Series)):
             for batch_start in range(0, len(it), batch_size):
                 yield it.iloc[batch_start:batch_start+batch_size]
             return
-        elif isinstance(it, (list, np.ndarray, h5py.Dataset)):
+
+        if isinstance(it, (list, np.ndarray, h5py.Dataset)):
             for batch_start in range(0, len(it), batch_size):
                 yield it[batch_start:batch_start+batch_size]
             return
-        else:
-            if not isinstance(it, Iterator):
-                assert isinstance(it, Iterable)
-                it = iter(it)
-    
-            while True:
-                batch = []
-                try:
-                    for _ in range(batch_size):
-                        batch.append(next(it))
-                except StopIteration:
-                    return
-                finally:
-                    if batch:
-                        yield batch
+
+        if not isinstance(it, Iterator):
+            assert isinstance(it, Iterable)
+            it = iter(it)
+
+        while True:
+            batch = []
+            try:
+                for _ in range(batch_size):
+                    batch.append(next(it))
+            except StopIteration:
+                return
+            finally:
+                if batch:
+                    yield batch
 
 
 class _iter_batches_with_len(_iter_batches):
