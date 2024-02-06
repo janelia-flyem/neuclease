3d2bfe1b30cb1b5882fdac270f210d88ec239f2f
Stuart Berg <bergs@janelia.hhmi.org>
2024-02-06 17:03:40 -0500

    PointLabeler: Better fast paths

--- neuclease/dvid/labelmap/pointlabeler.py
+++ neuclease/dvid/labelmap/pointlabeler.py
@@ -1,8 +1,11 @@
 import logging
+from collections import namedtuple
 
-from ._labelmap import fetch_mapping, fetch_mappings, fetch_mutations, fetch_bodies_for_many_points
+from ...util import Timer
+from ._labelmap import fetch_mapping, fetch_mappings, fetch_mutations, fetch_labels_batched, fetch_bodies_for_many_points
 
 logger = logging.getLogger(__name__)
+DvidSeg = namedtuple('DvidSeg', 'server uuid instance')
 
 
 class PointLabeler:
@@ -13,27 +16,39 @@ class PointLabeler:
     """
 
     def __init__(self, server, uuid, instance, mutations=None, mapping=None):
-        self.dvid_seg = (server, uuid, instance)
+        self.dvidseg = DvidSeg(server, uuid, instance)
         self._mutations = mutations
         self._mapping = mapping
 
     def update_bodies_for_points(self, point_df, batch_size=10_000, threads=0, processes=0):
         """
-        Works in-place. Adds a 'body' column to point_df, and also a
+        Works in-place. Adds a 'body' and 'sv' columns to point_df.
         """
         if len(point_df) < 1_000_000 and 'sv' in point_df.columns:
             # This fast path is convenient for small point lists (especially testing)
-            point_df['body'] = fetch_mapping(*self.dvid_seg, point_df['sv'].values, batch_size=batch_size, threads=max(threads, processes))
+            point_df['body'] = fetch_mapping(*self.dvidseg, point_df['sv'].values,
+                                             batch_size=batch_size, threads=max(threads, processes))
+        elif len(point_df) < 200_000:
+            # This fast path is convenient for small point lists (especially testing)
+            point_df['sv'] = fetch_labels_batched(*self.dvidseg, point_df[[*'zyx']].values, supervoxels=True,
+                                                  batch_size=batch_size, threads=max(threads, processes))
+            # This fast path is convenient for small point lists (especially testing)
+            point_df['body'] = fetch_mapping(*self.dvidseg, point_df['sv'].values,
+                                             batch_size=batch_size, threads=max(threads, processes))
         else:
             # This adds/updates 'sv' and 'body' columns to point_df
-            fetch_bodies_for_many_points(*self.dvid_seg, point_df, self.mutations, self.mapping, batch_size=batch_size, threads=threads, processes=processes)
+            fetch_bodies_for_many_points(*self.dvidseg, point_df, self.mutations, self.mapping,
+                                         batch_size=batch_size, threads=threads, processes=processes)
 
+    @property
     def mapping(self):
         if self._mapping is None:
-            self._mapping = fetch_mappings(*self.dvid_seg)
+            self._mapping = fetch_mappings(*self.dvidseg)
         return self._mapping
 
+    @property
     def mutations(self):
         if self._mutations is None:
-            self._mutations = fetch_mutations(*self.dvid_seg)
+            with Timer(f"Fetching full mutation log for {self.dvidseg.instance} from {self.dvidseg.uuid} and ancestors", logger):
+                self._mutations = fetch_mutations(*self.dvidseg)
         return self._mutations
