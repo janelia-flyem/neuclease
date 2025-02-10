92a0461e396449391445a221e377195968bf77b2
Stuart Berg <bergs@janelia.hhmi.org>
2025-02-09 19:03:19 -0500

    synapses: points_to_full_partner_table(): Drop rows in the partner table that aren't present in the points

--- neuclease/dvid/annotation.py
+++ neuclease/dvid/annotation.py
@@ -2403,9 +2403,14 @@ def partner_table_to_synapse_table(partner_df):
 
 
 def points_to_full_partner_table(point_df, partner_df):
+    """
+    Add columns from the point table to the partner table,
+    while simultaneously dropping any rows from the partner table
+    that don't have a corresponding row in the point table.
+    """
     assert point_df.index.name == 'point_id'
-    partner_df = partner_df.merge(point_df.rename_axis('pre_id'), 'left', on='pre_id')
-    partner_df = partner_df.merge(point_df.rename_axis('post_id'), 'left', on='post_id', suffixes=['_pre', '_post'])
+    partner_df = partner_df.merge(point_df.rename_axis('pre_id'), 'inner', on='pre_id')
+    partner_df = partner_df.merge(point_df.rename_axis('post_id'), 'inner', on='post_id', suffixes=['_pre', '_post'])
     return partner_df
 
 
