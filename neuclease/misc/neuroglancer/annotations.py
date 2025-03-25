b1f61178be252ae683d55b1ed7b3309dbf889377
Stuart Berg <bergs@janelia.hhmi.org>
2025-03-25 10:22:30 -0400

    misc.neuroglancer.annotations: Ensure that annotation 'id' is always a string (or else neuroglancer won't load it)

--- neuclease/misc/neuroglancer/annotations.py
+++ neuclease/misc/neuroglancer/annotations.py
@@ -299,9 +299,13 @@ def _standardize_annotation_dataframe(df):
         if col not in df.columns:
             df[col] = np.nan
 
-    if 'id' not in df.columns:
-        ids = [str(hex(abs(hash(tuple(x))))) for x in df[id_cols].values.tolist()]
-        df['id'] = ids
+    if 'id' in df.columns:
+        df['id'] = df['id'].astype(str)
+    else:
+        df['id'] = [
+            str(hex(abs(hash(tuple(x)))))
+            for x in df[id_cols].values.tolist()
+        ]
 
     is_point_or_ellipsoid = df[[*'xyz']].notnull().all(axis=1)
     is_line_or_box = df[['xa', 'ya', 'za', 'xb', 'yb', 'zb']].notnull().all(axis=1)
@@ -317,7 +321,7 @@ def _standardize_annotation_dataframe(df):
 
     # We have no way of choosing between 'line' and 'axis_aligned_bounding_box'
     # unless the user provides the 'type' explicitly.
-    # We default to 'axis_aligned_bounding_box'.
+    # We default to 'axis_aligned_bounding_box' because it's harder to type than 'line' :-)
     df['type'] = df['type'].fillna(
         df['x'].isnull().map({
             True: 'axis_aligned_bounding_box',
